7f69823f44281c6469bbc206c4ea2da222a16246
Voblit <michael.zakharov.physics@gmail.com>
2026-05-24 20:10:54 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,27 +1,39 @@
-import httpProxy from 'http-proxy';
+export default async function handler(req, res) {
 
-const proxy = httpProxy.createProxyServer({
-  target: 'https://cdn.startmyeducation.us',
-  changeOrigin: true,
-  autoRewrite: true,
-});
+  const trailingPath = req.url.replace(/^\/game-proxy/, '');
+  const targetUrl = `https://cdn.startmyeducation.us${trailingPath}`;
 
-export default function handler(req, res) {
-  req.url = req.url.replace(/^\/api\/proxy/, '');
-
-  return new Promise((resolve, reject) => {
-    proxy.web(req, res, {}, (err) => {
-      if (err) {
-        res.status(500).send('Proxy error: ' + err.message);
-        return reject(err);
-      }
-      resolve();
+  try {
+    const response = await fetch(targetUrl, {
+      method: req.method,
+      headers: {
+        'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
+        'Accept': req.headers['accept'] || '*/*',
+      },
     });
-  });
+
+    if (!response.ok) {
+      return res.status(response.status).send(`Target origin error: ${response.status}`);
+    }
+
+
+    const contentType = response.headers.get('content-type');
+    if (contentType) {
+      res.setHeader('Content-Type', contentType);
+    }
+
+
+    const data = await response.arrayBuffer();
+    return res.send(Buffer.from(data));
+
+  } catch (error) {
+    return res.status(500).send('Proxy failure: ' + error.message);
+  }
 }
+
 
 export const config = {
   api: {
-    bodyParser: false, 
+    bodyParser: false,
   },
 };
